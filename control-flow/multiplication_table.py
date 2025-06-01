5bfb4c7e88d7c39a91237d2e3f326f56b1afa56e
Sackitey68 <sackitey68@gmail.com>
2025-06-01 15:17:38 -0700

    This script will ask the user to enter a number, then use a for loop to print the multiplication table for that number from 1 to 10

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter a number
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and print the multiplication table using a for loop
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} * {i} = {result}")
+
