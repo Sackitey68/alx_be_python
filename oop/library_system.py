6b276a758c6811422a17fe1c45bb8162d286cfb5
sackitey68 <sackitey68@gmail.com>
2025-06-22 16:59:13 -0700

    Task 2 fixed

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,16 +3,16 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
-        self.file_size = file_size  # in KB
+        self.file_size = file_size
 
-    def get_details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -21,21 +21,17 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
     def __init__(self):
-        self.books = []  # Composition: manages a collection of books
+        self.books = []
 
     def add_book(self, book):
-        if isinstance(book, Book):
-            self.books.append(book)
-        else:
-            raise TypeError("Only instances of Book or its subclasses can be added.")
+        self.books.append(book)
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
-
+            print(book)
